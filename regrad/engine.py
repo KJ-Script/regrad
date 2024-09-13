8ae8db974570da972e2e27494f20ded62da2f69f
Keti Yohannes <ketiyohannes@gmail.com>
2024-09-13 14:14:20 +0300

    relu and sigmoid

--- regrad/engine.py
+++ regrad/engine.py
@@ -2,14 +2,14 @@ import math
 
 
 class Tensor:
-    
+
 
     def __init__(self, data):
         self.data = data
         self.grad = 0.0
         self._backward = lambda:None
 
-
+    
     def __repr__(self):
         return f"tensor({self.data}, {self.grad})"
     
@@ -28,7 +28,7 @@ class Tensor:
         
         return out
 
-
+    
     def __mul__(self, other):
         # forward pass
         other = other if isinstance(other, Tensor) else Tensor(other)
@@ -48,21 +48,29 @@ class Tensor:
         t= math.exp(2*n) - 1 / math.exp(2*n) + 1
         out = Tensor(t)
         
-
+       
         def _backward():
             self.grad += (1 - t**2) * out.grad
-
-        out._backward = _backward
-
-
-        return out
     
-
-
+        out._backward = _backward
+        return out
+   
     def __sigmoid__(self):
         n = self.data
         t = 1 / 1 + math.exp( -1 * n )
+        out  = Tensor(t)
 
 
         def _backward():
-            pass
+            self.grad += out.grad * (out.data * (1 - out.data))
+
+        out._backward = _backward
+
+            
+    def __relu__(self):
+        out = Tensor(0 of self.data < 0 else self.data)
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+
+        out._backward = _backward
